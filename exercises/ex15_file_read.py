31d6fe1946db581d8f21d84da0683d5d1fdcfcbb
Yevhen Strazhko <strazhkoe@gmail.com>
2022-07-24 13:32:22 +0300

    Ex 15 - simplest file reading

---
+++ exercises/ex15_file_read.py
@@ -0,0 +1,15 @@
+from sys import argv
+
+
+script, filename = argv
+
+txt = open(filename)
+
+print(f"Here is your file {filename}")
+print(txt.read())
+
+print("type file name to read:")
+filename = input('> ')
+
+txt = open(filename)
+print(f"Here is a text: {txt.read()}")
