113a02b7fc1da38541bc00c6a5e83508cfb122ee
Yevhen Strazhko <strazhkoe@gmail.com>
2022-07-24 13:52:35 +0300

    Closing files in ex 15

--- exercises/ex15_file_read.py
+++ exercises/ex15_file_read.py
@@ -8,8 +8,12 @@ txt = open(filename)
 print(f"Here is your file {filename}")
 print(txt.read())
 
+txt.close()
+
 print("type file name to read:")
 filename = input('> ')
 
 txt = open(filename)
 print(f"Here is a text: {txt.read()}")
+
+txt.close()
